6805fd934c4ecac833fcc20d00958b969afeb17d
DmitryKosaty <dkosaty@gmail.com>
2016-04-22 03:42:40 +0300

    Added points cloud of the first pair in the main coordinate system

--- main.py
+++ main.py
@@ -44,43 +44,43 @@ def main():
 
         reconstruct(pair_1=data[0], pair_2=data[2], filename='pair_1',
                     params={'min_disp':976, 'max_disp':1264},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+                    limits={'X':(0,10), 'Y':(-5,3), 'Z':(5,15)})
 
-        reconstruct(pair_1=data[1], pair_2=data[2], filename='pair_2',
-                    params={'min_disp':864, 'max_disp':1360},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
-
-        reconstruct(pair_1=data[2], pair_2=data[3], filename='pair_3',
-                    params={'min_disp':544, 'max_disp':768},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
-
-        reconstruct(pair_1=data[2], pair_2=data[4], filename='pair_4',
-                    params={'min_disp':480, 'max_disp':1184},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+        # reconstruct(pair_1=data[1], pair_2=data[2], filename='pair_2',
+        #             params={'min_disp':864, 'max_disp':1360},
+        #             limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+        #
+        # reconstruct(pair_1=data[2], pair_2=data[3], filename='pair_3',
+        #             params={'min_disp':544, 'max_disp':768},
+        #             limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+        #
+        # reconstruct(pair_1=data[2], pair_2=data[4], filename='pair_4',
+        #             params={'min_disp':480, 'max_disp':1184},
+        #             limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
 
         # reconstruct(pair_1=data[3], pair_2=data[4], filename='pair_5',
         #             params={'min_disp':608, 'max_disp':672},
         #             limits={'X':(-5,5), 'Y':(-5,5), 'Z':(10,13)})
 
-        reconstruct(pair_1=data[3], pair_2=data[5], filename='pair_6',
-                    params={'min_disp':1152, 'max_disp':1408},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+        # reconstruct(pair_1=data[3], pair_2=data[5], filename='pair_6',
+        #             params={'min_disp':1152, 'max_disp':1408},
+        #             limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
 
         # reconstruct(pair_1=data[4], pair_2=data[5], filename='pair_7',
         #             params={'min_disp':704, 'max_disp':832},
         #             limits={'X':(-11,0), 'Y':(-5,5), 'Z':(10,13)})
 
-        reconstruct(pair_1=data[5], pair_2=data[6], filename='pair_8',
-                    params={'min_disp':496, 'max_disp':720},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
-
-        reconstruct(pair_1=data[5], pair_2=data[7], filename='pair_9',
-                    params={'min_disp':1008, 'max_disp':1424},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
-
-        reconstruct(pair_1=data[6], pair_2=data[7], filename='pair_10',
-                    params={'min_disp':544, 'max_disp':704},
-                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+        # reconstruct(pair_1=data[5], pair_2=data[6], filename='pair_8',
+        #             params={'min_disp':496, 'max_disp':720},
+        #             limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+        #
+        # reconstruct(pair_1=data[5], pair_2=data[7], filename='pair_9',
+        #             params={'min_disp':1008, 'max_disp':1424},
+        #             limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+        #
+        # reconstruct(pair_1=data[6], pair_2=data[7], filename='pair_10',
+        #             params={'min_disp':544, 'max_disp':704},
+        #             limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
 
     except RuntimeError as error:
         print error
@@ -88,17 +88,17 @@ def main():
 def reconstruct(pair_1, pair_2, filename, params, limits):
     print 'PROCESSING OF THE '+filename
 
-    pair = StereoPair\
+    stereo_pair = StereoPair\
         (
             pair_1=pair_1,
             pair_2=pair_2,
             params=params
         )
-    pair.saveData(filename='work materials/'+filename+'/')
-    # points_cloud = pair.getPointsCloud(limits=limits)
+    stereo_pair.saveData(filename='work materials/'+filename+'/')
+    points_cloud = stereo_pair.getPointsCloud(limits=limits)
     # points_cloud.writeToFile(filename='work materials/'+filename+'/out.ply')
-    # points_cloud = pair.transformPCtoMainCoordinateSystem(points_cloud)
-    # points_cloud.writeToFile(filename='work materials/'+filename+'/out.ply')
+    points_cloud = stereo_pair.transformPCtoMainCoordinateSystem(points_cloud)
+    points_cloud.writeToFile(filename='work materials/'+filename+'/out.ply')
 
 if __name__ == "__main__":
     main()
