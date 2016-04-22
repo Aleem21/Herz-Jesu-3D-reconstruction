d5a81bd1f7ee7a044090b271846d8427e60b61d5
DmitryKosaty <dkosaty@gmail.com>
2016-04-22 03:12:07 +0300

    Selected all possible stereo pairs

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ folder, image_extension, camera_extension, projection_matrix_extension = 'herzje
 
 def main():
     try:
+        image_0 = cv2.imread(filename=folder+'0000'+image_extension)
         image_1 = cv2.imread(filename=folder+'0001'+image_extension)
         image_2 = cv2.imread(filename=folder+'0002'+image_extension)
         image_3 = cv2.imread(filename=folder+'0003'+image_extension)
-        image_4 = cv2.imread(filename=folder+'0004'+image_extension) # it's the head frame
+        image_4 = cv2.imread(filename=folder+'0004'+image_extension)
         image_5 = cv2.imread(filename=folder+'0005'+image_extension)
         image_6 = cv2.imread(filename=folder+'0006'+image_extension)
         image_7 = cv2.imread(filename=folder+'0007'+image_extension)
@@ -17,6 +18,7 @@ def main():
         print error.message
 
     try:
+        camera_0 = Camera(filename='0000')
         camera_1 = Camera(filename='0001')
         camera_2 = Camera(filename='0002')
         camera_3 = Camera(filename='0003')
@@ -30,6 +32,7 @@ def main():
     try:
         data = \
             {
+                0: (image_0, camera_0),
                 1: (image_1, camera_1),
                 2: (image_2, camera_2),
                 3: (image_3, camera_3),
@@ -38,36 +41,64 @@ def main():
                 6: (image_6, camera_6),
                 7: (image_7, camera_7)
             }
-        reconstruct(data)
+
+        reconstruct(pair_1=data[0], pair_2=data[2], filename='pair_1',
+                    params={'min_disp':976, 'max_disp':1264},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
+        reconstruct(pair_1=data[1], pair_2=data[2], filename='pair_2',
+                    params={'min_disp':864, 'max_disp':1360},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
+        reconstruct(pair_1=data[2], pair_2=data[3], filename='pair_3',
+                    params={'min_disp':544, 'max_disp':768},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
+        reconstruct(pair_1=data[2], pair_2=data[4], filename='pair_4',
+                    params={'min_disp':480, 'max_disp':1184},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
+        # reconstruct(pair_1=data[3], pair_2=data[4], filename='pair_5',
+        #             params={'min_disp':608, 'max_disp':672},
+        #             limits={'X':(-5,5), 'Y':(-5,5), 'Z':(10,13)})
+
+        reconstruct(pair_1=data[3], pair_2=data[5], filename='pair_6',
+                    params={'min_disp':1152, 'max_disp':1408},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
+        # reconstruct(pair_1=data[4], pair_2=data[5], filename='pair_7',
+        #             params={'min_disp':704, 'max_disp':832},
+        #             limits={'X':(-11,0), 'Y':(-5,5), 'Z':(10,13)})
+
+        reconstruct(pair_1=data[5], pair_2=data[6], filename='pair_8',
+                    params={'min_disp':496, 'max_disp':720},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
+        reconstruct(pair_1=data[5], pair_2=data[7], filename='pair_9',
+                    params={'min_disp':1008, 'max_disp':1424},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
+        reconstruct(pair_1=data[6], pair_2=data[7], filename='pair_10',
+                    params={'min_disp':544, 'max_disp':704},
+                    limits={'X':(-50,50), 'Y':(-50,50), 'Z':(-50,50)})
+
     except RuntimeError as error:
         print error
 
-def reconstruct(data):
-    print 'PROCESSING OF THE FIRST PAIR'
-    pair_1 = StereoPair\
-        (
-            pair_1=data[3],
-            pair_2=data[4],
-            params={'min_disp':608, 'max_disp':672}
-        )
-    pair_1.saveData(filename='work materials/pair_1/')
-    points_cloud = pair_1.getPointsCloud(limits={'X':(-5,5), 'Y':(-5,5), 'Z':(10,13)})
-    # points_cloud.writeToFile(filename='work materials/pair_1/out.ply')
-    points_cloud = pair_1.transformPCtoMainCoordinateSystem(points_cloud)
-    points_cloud.writeToFile(filename='work materials/pair_1/out.ply')
+def reconstruct(pair_1, pair_2, filename, params, limits):
+    print 'PROCESSING OF THE '+filename
 
-    print 'PROCESSING OF THE SECOND PAIR'
-    pair_2 = StereoPair\
+    pair = StereoPair\
         (
-            pair_1=data[4],
-            pair_2=data[5],
-            params={'min_disp':704, 'max_disp':832}
+            pair_1=pair_1,
+            pair_2=pair_2,
+            params=params
         )
-    pair_2.saveData(filename='work materials/pair_2/')
-    points_cloud = pair_2.getPointsCloud(limits={'X':(-11,0), 'Y':(-5,5), 'Z':(10,13)})
-    # points_cloud.writeToFile(filename='work materials/pair_2/out.ply')
-    points_cloud = pair_2.transformPCtoMainCoordinateSystem(points_cloud)
-    points_cloud.writeToFile(filename='work materials/pair_2/out.ply')
+    pair.saveData(filename='work materials/'+filename+'/')
+    # points_cloud = pair.getPointsCloud(limits=limits)
+    # points_cloud.writeToFile(filename='work materials/'+filename+'/out.ply')
+    # points_cloud = pair.transformPCtoMainCoordinateSystem(points_cloud)
+    # points_cloud.writeToFile(filename='work materials/'+filename+'/out.ply')
 
 if __name__ == "__main__":
     main()
